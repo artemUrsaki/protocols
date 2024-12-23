bacd85ef135f9e6ad0122fb68146022671eeb041
artemUrsaki <artem06ursaki@gmail.com>
2024-12-23 15:37:25 +0100

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/*.vue', './src/components/*.vue', './src/views/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'accent-blue': '#0077ff',
+        'dark-blue': '#000c1a',
+        'accent-purple': '#5e00ff',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
